6df9ef6a92248b0ec9f18f714b4e4c0adef7198b
luozhen <15902773450@163.com>
2018-08-09 18:21:51 +0800

    楼盘关键字列表加入城市,区域,商圈,楼盘搜索

--- app/Repositories/BuildingKeywordsRepository.php
+++ app/Repositories/BuildingKeywordsRepository.php
@@ -2,6 +2,8 @@
 
 namespace App\Repositories;
 
+use App\Models\Area;
+use App\Models\Block;
 use App\Models\BuildingKeyword;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,7 +14,29 @@ class BuildingKeywordsRepository extends Model
         $request
     )
     {
-        return BuildingKeyword::paginate($request->per_page??10);
+        $result = BuildingKeyword::make();
+
+        if ($request->city_guid) {
+            // 通过城市查出区域
+            $areaGuids = Area::where('city_guid', $request->city_guid)->pluck('guid')->toArray();
+            $areas = Area::whereIn('guid', $areaGuids)->with('building')->get();
+            $buildingGuid = $areas->map(function($area) {
+                return [
+                    array_column(Area::find($area->guid)->building->flatten()->toArray(), 'guid')
+                ];
+            });
+            $result = $result->whereIn('building_guid', $buildingGuid->flatten()->toArray());
+        } elseif ($request->area_guid) {
+            $buildingGuid = array_column(Area::find($request->area_guid)->building->flatten()->toArray(), 'guid');
+            $result = $result->whereIn('building_guid', $buildingGuid);
+        } elseif ($request->block_guid) {
+            $buildingGuid = array_column(Block::find($request->block_guid)->building->flatten()->toArray(), 'guid');
+            $result = $result->whereIn('building_guid', $buildingGuid);
+        } elseif ($request->building_guid) {
+            $result = $result->where(['building_guid' => $request->building_guid]);
+        }
+
+        return $result->paginate($request->per_page??10);
     }
 
     // 修改关键字
