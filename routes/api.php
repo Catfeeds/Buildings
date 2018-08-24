126f8b029e01a5ad14fbc5dc11703890025d5568
luozhen <15902773450@163.com>
2018-08-24 15:44:50 +0800

    合并zy

--- routes/api.php
+++ routes/api.php
@@ -8,59 +8,57 @@ Route::group(['namespace' => 'API'], function () {
     // 登录
     Route::resource('/login', 'LoginController');
 
-    // 所有下拉
-    Route::get('get_all_select', 'CitiesController@getAllSelect');
+    Route::group(['middleware' => ['auth:api']], function () {
+            // 退出
+            Route::post('logout', 'LoginController@logout');
+            // 用户
+            Route::resource('/users', 'UsersController');
+            // 七牛token
+            Route::get('/get_qi_niu_token', 'CitiesController@token');
+            // 城市
+            Route::resource('/cities', 'CitiesController');
+            // 获取所有城市
+            Route::get('/get_all_city', 'CitiesController@getAllCity');
+            // 区域
+            Route::resource('/areas', 'AreasController');
+            // 获取所有区域
+            Route::get('/get_all_area', 'AreasController@getAllArea');
+            // 所有区的下拉数据
+            Route::get('/areas_select', 'AreasController@areasSelect');
+            // 商圈
+            Route::resource('/blocks', 'BlocksController');
+            // 某区域下拉数据
+            Route::get('/blocks_select', 'BlocksController@blocksSelect');
+            // 获取所有商圈基础地理位置
+            Route::get('block_locations', 'BlocksController@blockLocations');
+            // 获取所有商圈
+            Route::get('all_building_blocks', 'BlocksController@allBuildingBlock');
+            // 楼盘
+            Route::resource('/buildings', 'BuildingsController');
+            // 楼盘下拉
+            Route::get('/buildings_select', 'BuildingsController@buildingSelect');
+            // 楼盘搜索
+            Route::get('/building_search_select', 'BuildingsController@buildingSearchSelect');
 
-//    Route::group(['middleware' => ['auth:api']], function () {
-        // 退出
-        Route::post('logout','LoginController@logout');
-        // 用户
-        Route::resource('/users', 'UsersController');
-        // 七牛token
-        Route::get('/get_qi_niu_token', 'CitiesController@token');
-        // 城市
-        Route::resource('/cities', 'CitiesController');
-        // 获取所有城市
-        Route::get('/get_all_city', 'CitiesController@getAllCity');
-        // 区域
-        Route::resource('/areas', 'AreasController');
-        // 获取所有区域
-        Route::get('/get_all_area', 'AreasController@getAllArea');
-        // 所有区的下拉数据
-        Route::get('/areas_select', 'AreasController@areasSelect');
-        // 商圈
-        Route::resource('/blocks', 'BlocksController');
-        // 某区域下拉数据
-        Route::get('/blocks_select', 'BlocksController@blocksSelect');
-        // 获取所有商圈基础地理位置
-        Route::get('block_locations', 'BlocksController@blockLocations');
-        // 获取所有商圈
-        Route::get('all_building_blocks', 'BlocksController@allBuildingBlock');
-        // 楼盘
-        Route::resource('/buildings', 'BuildingsController');
-        // 楼盘下拉
-        Route::get('/buildings_select', 'BuildingsController@buildingSelect');
-        // 楼盘搜索
-        Route::get('/building_search_select', 'BuildingsController@buildingSearchSelect');
+            // 拿到楼盘下的所有楼座
+            Route::resource('/building_blocks', 'BuildingBlocksController');
+            // 所有的楼座下拉数据
+            Route::get('/building_blocks_all', 'BuildingBlocksController@buildingBlocksSelect');
+            // 楼座分页列表
+            Route::get('/building_blocks_list', 'BuildingBlocksController@allBlocks');
+            // 修改某个楼座的名称
+            Route::post('/change_name_unit/{building_block}', 'BuildingBlocksController@changeNameUnit');
+            // 添加楼座（名称、楼盘）
+            Route::post('/add_name_unit', 'BuildingBlocksController@addNameUnit');
+            // 补充楼座信息
+            Route::post('/add_block_info/{building_block}', 'BuildingBlocksController@addBlockInfo');
 
-        // 拿到楼盘下的所有楼座
-        Route::resource('/building_blocks', 'BuildingBlocksController');
-        // 所有的楼座下拉数据
-        Route::get('/building_blocks_all', 'BuildingBlocksController@buildingBlocksSelect');
-        // 楼座分页列表
-        Route::get('/building_blocks_list', 'BuildingBlocksController@allBlocks');
-        // 修改某个楼座的名称
-        Route::post('/change_name_unit/{building_block}', 'BuildingBlocksController@changeNameUnit');
-        // 添加楼座（名称、楼盘）
-        Route::post('/add_name_unit', 'BuildingBlocksController@addNameUnit');
-        // 补充楼座信息
-        Route::post('/add_block_info/{building_block}', 'BuildingBlocksController@addBlockInfo');
+            // 通过楼座获取城市
+            Route::get('/adopt_building_block_get_city', 'BuildingBlocksController@adoptBuildingBlockGetCity');
 
-        // 通过楼座获取城市
-        Route::get('/adopt_building_block_get_city', 'BuildingBlocksController@adoptBuildingBlockGetCity');
-
-        // 关键字管理
-        Route::resource('/building_keywords', 'BuildingKeywordsController');
+            // 关键字管理
+            Route::resource('/building_keywords', 'BuildingKeywordsController');
+        });
 
 
         // 安全验证
