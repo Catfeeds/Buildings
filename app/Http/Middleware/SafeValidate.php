f96ec10f3a528ff24bc1f1ec535dadfdb4234283
luozhen <15902773450@163.com>
2018-08-24 17:01:55 +0800

    安全验证

--- app/Http/Middleware/SafeValidate.php
+++ app/Http/Middleware/SafeValidate.php
@@ -8,19 +8,18 @@ use Illuminate\Support\Facades\Hash;
 class SafeValidate
 {
     // 安全验证
-    public function handle
-    (
+    public function handle(
         $request,
         Closure $next
     )
     {
         try {
-            if (!Hash::check('chulouwang'.date('Y-m-d',time()),$request->header('safeString'))) {
-                return response()->json(["message" => "认证失败"],401);
+            if (!Hash::check('chulouwang'.date('Y-m-d',time()), $request->header('safeString'))) {
+                return response()->json(["message" => "认证失败"], 401);
             }
         } catch (\Exception $e) {
             \Log::info($e->getFile().$e->getLine().$e->getMessage());
-            return response()->json(["message" => "认证失败"],401);
+            return response()->json(["message" => "认证失败"], 401);
         }
 
         return $next($request);
