125bd76b5566dbff8a8b4bcbc3e06e025a6b83dd
刘坤涛 <287667404@qq.com>
2018-09-27 07:49:09 +0000

    修改

--- app/AgencyModels/AgencyBuilding.php
+++ app/AgencyModels/AgencyBuilding.php
@@ -12,6 +12,12 @@ class AgencyBuilding extends Model
 
     protected $connection = 'agency';
 
+    protected $casts = [
+        'gps' => 'array',
+        'album' => 'array',
+        'big_album' => 'array',
+    ];
+
     // 楼座
     public function agencyBuildingBlock()
     {
